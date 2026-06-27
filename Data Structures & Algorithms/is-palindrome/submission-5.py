0d42bae4b6cca87980dce61d54d0dd3277e77a80
Lalit Boyapati <lalitroy234@gmail.com>
2026-06-27 16:50:30 -0400

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        news = ""
+        for x in s: 
+            if x.isalnum(): news += x.lower()
+        return news == news[::-1]
+
+        
